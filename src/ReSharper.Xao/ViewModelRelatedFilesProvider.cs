b3cc37f471a63da72a636355625f267b903fe389
Igal Tabachnik <hmemcpy@gmail.com>
2014-03-04 23:52:46 +0200

    Navigation View<->ViewModel (based on View/ViewModel suffixes)

--- src/ReSharper.Xao/ViewModelRelatedFilesProvider.cs
+++ src/ReSharper.Xao/ViewModelRelatedFilesProvider.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using JetBrains.Metadata.Reader.API;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.Navigation.GoToRelated;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.Caches;
+using JetBrains.ReSharper.Psi.Modules;
 using JetBrains.ReSharper.Psi.Tree;
 using JetBrains.Util;
 
@@ -14,10 +16,31 @@ namespace ReSharper.Xao
     [RelatedFilesProvider(typeof(KnownProjectFileType))]
     public class ViewModelRelatedFilesProvider : IRelatedFilesProvider
     {
+        private enum FileKind
+        {
+            None,
+            View,
+            ViewModel
+        }
+
         public IEnumerable<JetTuple<IProjectFile, string, IProjectFile>> GetRelatedFiles(IProjectFile projectFile)
         {
-            string viewName = projectFile.Name.Substring(0, projectFile.Name.LastIndexOf("View", StringComparison.Ordinal));
-            IEnumerable<IClrDeclaredElement> searchResults = FindType(projectFile.GetSolution(), viewName + "ViewModel");
+            FileKind kind = GetFileKind(projectFile);
+            int suffixStart;
+            switch (kind)
+            {
+                case FileKind.View:
+                case FileKind.ViewModel:
+                    suffixStart = projectFile.Name.LastIndexOf(kind.ToString(), StringComparison.OrdinalIgnoreCase);
+                    break;
+                default:
+                    return EmptyList<JetTuple<IProjectFile, string, IProjectFile>>.InstanceList;
+            }
+
+            string typeName = projectFile.Name.Substring(0, suffixStart);
+            string newSuffix = kind == FileKind.View ? "ViewModel" : "View";
+
+            IEnumerable<IClrDeclaredElement> searchResults = FindType(projectFile.GetSolution(), typeName + newSuffix);
             IEnumerable<IPsiSourceFile> sourceFiles = searchResults.SelectMany(element => element.GetSourceFiles());
 
             var elementCollector = new RecursiveElementCollector<ITypeDeclaration>();
@@ -28,7 +51,28 @@ namespace ReSharper.Xao
 
             return elementCollector.GetResults()
                                    .Select(declaration => declaration.GetSourceFile().ToProjectFile())
-                                   .Select(file => JetTuple.Of(file, "ViewModel", projectFile));
+                                   .Select(file => JetTuple.Of(file, newSuffix, projectFile));
+        }
+
+        private FileKind GetFileKind(IProjectFile projectFile)
+        {
+            IEnumerable<string> typeNamesInFile = GetTypeNamesDefinedInFile(projectFile).ToList();
+            if (typeNamesInFile.Any(s => s.EndsWith("View", StringComparison.OrdinalIgnoreCase))) return FileKind.View;
+            if (typeNamesInFile.Any(s => s.EndsWith("ViewModel", StringComparison.OrdinalIgnoreCase))) return FileKind.ViewModel;
+            return FileKind.None;
+        }
+
+        private IEnumerable<string> GetTypeNamesDefinedInFile(IProjectFile projectFile)
+        {
+            IPsiSourceFile psiSourceFile = projectFile.ToSourceFile();
+            if (psiSourceFile == null)
+            {
+                return EmptyList<string>.InstanceList;
+            }
+
+            return psiSourceFile.GetPsiServices().Symbols.GetTypesAndNamespacesInFile(psiSourceFile)
+                                .OfType<ITypeElement>()
+                                .Select(element => element.ShortName);
         }
 
         private static IEnumerable<IClrDeclaredElement> FindType(ISolution solution, string typeToFind)
