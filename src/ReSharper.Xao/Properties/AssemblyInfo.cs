489edc9dc4be740357b604b249aaa11bdd6b6970
Igal Tabachnik <hmemcpy@gmail.com>
2014-03-04 13:52:10 +0200

    Adding project. Supporting both 8.1 and 8.2

---
+++ src/ReSharper.Xao/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using JetBrains.ActionManagement;
+using JetBrains.Application.PluginSupport;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ReSharper.Xao")]
+[assembly: AssemblyDescription("Plugin to navigate between a XAML View and ViewModel. Invoked with Go to Related Files (Ctrl+Shift+Alt+G).\r\n" +
+                               "Copyright © Igal Tabachnik, 2014")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Igal Tabachnik")]
+[assembly: AssemblyProduct("ReSharper.Xao")]
+[assembly: AssemblyCopyright("Copyright © Igal Tabachnik, 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
+
+// The following information is displayed by ReSharper in the Plugins dialog
+[assembly: PluginTitle("Xao")]
+[assembly: PluginDescription("Plugin to navigate between a XAML View and ViewModel. Invoked with Go to Related Files (Ctrl+Shift+Alt+G).\r\n" +
+                             "Copyright © Igal Tabachnik, 2014")]
+[assembly: PluginVendor("Igal Tabachnik")]
---
+++ src/ReSharper.Xao/ViewModelRelatedFilesProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Metadata.Reader.API;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.Navigation.GoToRelated;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Caches;
+using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.Util;
+
+namespace ReSharper.Xao
+{
+    [RelatedFilesProvider(typeof(KnownProjectFileType))]
+    public class ViewModelRelatedFilesProvider : IRelatedFilesProvider
+    {
+        public IEnumerable<JetTuple<IProjectFile, string, IProjectFile>> GetRelatedFiles(IProjectFile projectFile)
+        {
+            string viewName = projectFile.Name.Substring(0, projectFile.Name.LastIndexOf("View", StringComparison.Ordinal));
+            IEnumerable<IClrDeclaredElement> searchResults = FindType(projectFile.GetSolution(), viewName + "ViewModel");
+            IEnumerable<IPsiSourceFile> sourceFiles = searchResults.SelectMany(element => element.GetSourceFiles());
+
+            var elementCollector = new RecursiveElementCollector<ITypeDeclaration>();
+            foreach (IFile file in sourceFiles.SelectMany(psiSourceFile => psiSourceFile.EnumerateDominantPsiFiles()))
+            {
+                elementCollector.ProcessElement(file);
+            }
+
+            return elementCollector.GetResults()
+                                   .Select(declaration => declaration.GetSourceFile().ToProjectFile())
+                                   .Select(file => JetTuple.Of(file, "ViewModel", projectFile));
+        }
+
+        private static IEnumerable<IClrDeclaredElement> FindType(ISolution solution, string typeToFind)
+        {
+            ISymbolScope declarationsCache = solution.GetPsiServices().Symbols
+                .GetSymbolScope(LibrarySymbolScope.FULL, context: UniversalModuleReferenceContext.Instance, caseSensitive: false);
+            
+            List<IClrDeclaredElement> results = declarationsCache.GetElementsByShortName(typeToFind).ToList();
+            return results;
+        }
+    }
+}
